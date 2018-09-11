bb7fb2d6059cc1f1d1d130ea4886dc0dc292dc1c
Fatemeh.Ostad <fatemeh.ostad@mediasmiths.com>
2018-09-11 14:03:26 +0200

    added nav bar

---
+++ src/js/components/nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav
+      class="navbar navbar-default navbar-fixed-top"
+      style={{fontWeight: 'bold', fontSize: '14px', borderBottomWidth: '0px'}}
+    >
+      Media DB
+    </nav>
+  );
+};
+
+export default Nav;
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Route, Switch, BrowserRouter, IndexRoute } from 'react-router-dom';
 
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
@@ -8,6 +8,7 @@ import reducer from './reducers';
 
 import Search from "./components/search";
 import Movie from "./components/Movie";
+import Layout from "./components/Layout";
 
 const app = document.getElementById('root');
 const store = createStore(reducer);
@@ -16,8 +17,11 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <Switch>
-        <Route exact path='/' component={Search}/>
-        <Route exact path='/movie' component={Movie}/>
+        <Layout >
+          <Route exact path='/' component={Search}/>
+          <Route exact path='/search' component={Search}/>
+          <Route exact path='/movie' component={Movie}/>
+        </Layout >
       </Switch>
     </BrowserRouter>
   </Provider>, app);
